da88d68e6b50ed15e30ba21ea1f7108e1759bba4
Vinni Uba <20180015014@my.xu.edu.ph>
2024-02-11 21:12:55 +0800

    template #5

--- gallery.php
+++ gallery.php
@@ -7,7 +7,7 @@ include('config/dbcon.php');
 ?>
 <link rel="stylesheet" href="assets/css/custom.css">
 
-    <div class="container-fluid">
+    <div class="container-fluid custombg-image-row ">
         <div class="row">
             <!-- Sidebar -->
             <div class="col-md-3 fixed-left" style="width:350px">
@@ -15,41 +15,43 @@ include('config/dbcon.php');
             </div>
 
             <!-- Main Body -->
-            <div class="col-md-9 ">
-                <div class="container">
+            <div class="col-md-9">
+                <div class="">
                     <div class="row ">
                         <div class="col-md-12">
                             <?php include('message.php'); ?>
                                 <div class="card-header">
                                     <h4 class="card-title text-center customHome">Gallery</h4>
                                 </div>
-                                <div>
-                                    <!-- FETCH FROM DATABASE -->
-                                    <?php
-                                    $navbarCategory = "SELECT * FROM projects";
-                                    $navbarCategory_run = mysqli_query($con, $navbarCategory);
-                                    if(mysqli_num_rows($navbarCategory_run) > 0)
-                                    {
-                                        foreach($navbarCategory_run as $navItems)
-                                        {
-                                        ?>
-                                        <ul class="nav-item">
-                                            <a class="nav-item" href="projects.php?title=<?=$navItems['id'];?>"><?=$navItems['description'];?></a>
-                                        </ul>
-                                        <?php
-                                        }
-                                    }
-                                    ?>
-                                </div>
                         </div>
+                    <div class="row">
+                        <?php
+                        $query = "SELECT * FROM categories";
+                        $query_run = mysqli_query($con, $query);
+                        if(mysqli_num_rows($query_run) > 0)
+                        {
+                            foreach($query_run as $item)
+                            {
+                            ?>
+                            <div class="col-md-4 mb-4">
+                                <div class="card h-100">
+                                    <div class="card-body">
+                                        <h5 class="card-title"><?= $item['id']; ?></h5>
+                                        <p class="card-text"><?= $item['name']; ?></p>
+                                        <!-- You can add more project details here -->
+                                    </div>
+                                </div>
+                            </div>
+                            <?php
+                            }
+                        }
+                        ?>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
 <!-- Footer -->
-<footer class="footer">
 <?php include('includes/footer.php'); ?>
-</footer>
- 
