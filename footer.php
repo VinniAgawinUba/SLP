795ac25d211d189cdc78f251064e8057e3fd6136
Doquix <89464110+Doquix@users.noreply.github.com>
2024-02-21 20:39:48 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,60 @@
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap5.bundle.min.js"></script>
+<script src="assets/js/scripts.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.11.1/viewer.min.js"></script>
+                                        
+
+<!-- Page Content-->
+<section class="pt-4 bg-light bg-gradient">
+            <div class="container px-lg-5">
+                <!-- Page Features-->
+                <div class="row gx-lg-5">
+                    <div class="col-lg-5 col-xxl-4">
+                        <div class="card bg-light border-0 h-100">
+                            <div class="card-body p-4 p-lg-5 pt-0 pt-lg-0">
+                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-collection"></i></div>
+                                <h2 class="fs-4 fw-bold">CONTACT US</h2>
+                                <a href=""><p class="mb-0">rso@xu.edu.ph</p></a>
+                                <p class="mb-0">(088) 853-9800 | Local 9173</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-7 col-xxl-4">
+                        <div class="card bg-light border-0 h-100">
+                            <div class="card-body p-4 p-lg-5 pt-0 pt-lg-0">
+                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-cloud-download"></i></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-7 col-xxl-4">
+                        <div class="card bg-light border-0 h-100">
+                            <div class="card-body p-4 p-lg-5 pt-0 pt-lg-0">
+                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-cloud-download"></i></div>
+                                <h2 class="fs-4 fw-bold">REQUEST ACCESS</h2>
+                                <p class="mb-0">Want more access to information?</p>
+                                <button type="button" class="btn btn-primary">Request Access</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row gx-lg-5">
+                <div class="col-lg-7 col-xxl-4">
+                        <div class="card bg-light border-0 h-100">
+                            <div class="card-body p-4 p-lg-5 pt-0 pt-lg-0">
+                                <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-cloud-download"></i></div>
+                                <h2 class="fs-4 fw-bold">FIND US HERE</h2>
+                                <p class="mb-0">Ground Floor, Agriculture Building, Room 103, Xavier University - </p>
+                                <p class="mb-0">Ateneo de Cagayan, Corrales Avenue,</p>
+                                <p class="mb-0">Cagayan de Oro City, 9000, Philippines</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row text-center">
+                    <p>© 2023 Xavier University - Ateneo De Cagayan Corrales Avenue, Cagayan de Oro, Philippines</p>
+                </div>
+            </div>
+        </section>
+
+</body>
+</html>
--- projects.php
+++ projects.php
@@ -7,63 +7,37 @@ include('config/dbcon.php');
 ?>
 <link rel="stylesheet" href="assets/css/custom.css">
 
-<div class="container-fluid custombg-image-row">
-    <div class="row">
-        <!-- Sidebar -->
-        <div class="col-md-3 fixed-left" style="width:350px">
-            <?php include('includes/sidebar.php'); ?>
-        </div>
-
-        <!-- Main Body -->
-        <div class="col-md-9">
-            <div class="">
-                <div class="row ">
-                    <div class="col-md-12">
-                        <?php include('message.php'); ?>
-                        <div class="card-header">
-                            <h4 class="card-title text-center customHome">Projects</h4>
-                        </div>
-                    </div>
+    <div class="container-fluid custombg-image-row ">
+        <div class="row gy-3">
+            <!-- Main Body -->
+            <div class="col-3">
+            </div>
+            <div class="col-6">
+            <h4 class="card-title text-center customHome">Projects</h4>
+            </div>
+            <div class="col-3"></div>
+            <div class="mainContent">
                     <div class="row">
                         <?php
-                        $query1 = "SELECT * FROM school_year";
-                        $query1_run = mysqli_query($con, $query1);
-                        $query2 = "SELECT * FROM college";
-                        $query2_run = mysqli_query($con, $query2);
-                       
-                        
-                        // Construct the base URL
-                        $base_url = $_SERVER['PHP_SELF'] . "?";
-                        
-                        // Append existing parameters to the base URL
-                        $existing_params = http_build_query(array_intersect_key($_GET, array_flip(['school_year', 'college','department'])));
-                        if (!empty($existing_params)) {
-                            $base_url .= $existing_params . "&";
-                        }
-                        
-                        //If school year is already set as params render All Colleges
-                        if(mysqli_num_rows($query1_run) > 0) {
-                            if(isset($_GET['school_year']) && !isset($_GET['college_id']) && !isset($_GET['department_id'])) {
-                                if (mysqli_num_rows($query2_run) > 0){
-                                foreach($query2_run as $item) {
-                                    ?>
-                                    <div class="col-md-4 mb-4">
-                                        <div class="card h-100">
-                                            <div class="card-body">
-                                                <?php
-                                                $college = $item['id'];
-                                                $url = $base_url . "college_id=" . urlencode($college);
-                                                ?>
-                                                <a href="<?= $url ?>">
-                                                    <h5 class="card-title"><?= $item['name']; ?></h5>
-                                                    <p class="card-text"><?= $college; ?></p>
-                                                </a>
-                                            </div>
-                                        </div>
+                        $query = "SELECT * FROM posts";
+                        $query_run = mysqli_query($con, $query);
+                        if(mysqli_num_rows($query_run) > 0)
+                        {
+                            foreach($query_run as $item)
+                            {
+                            ?>
+                            <div class="col-md-3 mb-3 gy-3">
+                                <div class="card h-100" style="width: 20rem; margin-left: 2vw;">
+                                <a href="#"><img src="assets/images/vccineCrd.jpg" class="customPic"></a> <!-- Placeholder for image-->
+                                    <div class="card-body">
+                                        <h5 class="card-title"><?= $item['id']; ?></h5>
+                                        <p class="card-text"><?= $item['name']; ?></p>
+                                        <!-- You can add more project details here -->
                                     </div>
-                                    <?php
-                                }
-                                
+                                </div> 
+                            </div>
+                            
+                            <?php
                             }
                         }
                             //if School Year And College is Set as params render Departments
@@ -139,9 +113,10 @@ include('config/dbcon.php');
                            
                         } else {echo '<a style="color:white">No Records Found</a>';}
                     }
-                }
+                
                         ?>
                     </div>
+            </div>
                 </div>
             </div>
         </div>
