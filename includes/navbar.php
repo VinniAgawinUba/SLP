7b2dd5d284d7454dfea10da3d640dba95bb9f60a
C00L1N <jimenez.bencoolin@gmail.com>
2024-04-01 20:57:13 +0800

    Update navbar.php
    adding hover effects for navigation links and updated font style

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,95 +1,130 @@
-<div class="container-fluid px-2">
+<!DOCTYPE html>
+<html lang="en">
 
-    <!-- Navbar Top Right -->
-    <nav class="navbar navbar-expand-lg">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/bootstrap5.css">
 
-        <a class="navbar-brand d=block d-sm-none d-md-none" href="#"></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- XU Logo -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <style>
+        #custom-search {
+            width: 585.91px;
+            height: 43.82px;
+            background: rgba(40, 57, 113, 0.15);
+            border-radius: 22px;
+            letter-spacing: 3px;
+            font-family: 'Inter';
+            padding: 8px 12px;
+            border: 1px solid #ccc;
+        }
 
-            <a href="index.php" style="width:400px;">
-                <img src="assets/images/XU_LOGO.png" alt="Logo" style="width: 80%;">
+        ::placeholder {
+            color: #283971;
+            font-style: normal;
+            font-weight: 700;
+        }
 
-            </a>
+        .font-style {
+            font-family: 'Inter';
+            color: #283971;
+            font-weight: 700;
+            font-size: 15px;
+            text-align: center;
+            width: 177.72px;
+            height: 19.48px;
+            font-style: normal;
+            text-align: center;
+            margin-bottom: 30px;
+            letter-spacing: 3px;
+        }
 
-            <!-- Search Bar -->
-            <form class="d-flex justify-content-center ms-auto">
-                <input class="form-control me-2 custom-search" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-primary" type="submit">Search</button>
-            </form>
-            <!-- Navbar Links -->
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
 
+        hr {
+            width: 798px;
+            height: 4px;
+            left: calc(50% - 798px/2 - 8px);
 
+            background: #283971;
+            border-radius: 14px;
+        }
 
-                <?php if (isset($_SESSION['auth_user'])) : ?>
+        #nav-item:hover {
+            color: #A19158;
+            transition: color 0.3s
+        }
 
-                    <li class="nav-item dropdown customfont">
-                        <a class="nav-link dropdown-toggle customfont" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <?= $_SESSION['auth_user']['user_name']; ?>
-                        </a>
-                        <ul class="dropdown-menu customfont" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item customfont" href="#">My Profile</a></li>
-                            <li>
-                                <!-- If logged in show shortcut to admin panel -->
-                                <?php if ($_SESSION['auth_role'] == '1' || $_SESSION['auth_role'] == '2') {
-                                ?>
-                                    <a class="dropdown-item customfont" href="admin/index.php">Admin Panel</a>
-                                <?php
-                                } ?>
-                            </li>
-                            <li>
-                                <form action="allcode.php" method="post">
-                                    <button type="submit" name="logout_btn" class="dropdown-item">Logout</button>
-                                </form>
-                            </li>
-                        </ul>
+    </style>
+</head>
+
+<body>
+    <div class="container-fluid px-2">
+        <nav class="navbar navbar-expand-lg">
+            <a class="navbar-brand d-block d-sm-none d-md-none" href="#"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <a href="index.php" style="width:400px;">
+                    <img src="assets/images/XU_LOGO.png" alt="Logo" style="width: 80%;">
+                </a>
+                <form class="d-flex justify-content-center ms-auto">
+                    <input class="form-control me-2 custom-search" id="custom-search" type="search" placeholder="Search">
+                    <button class="btn btn-outline-primary" id="search-button" type="submit">Search</button>
+                </form>
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item text-center customfont">
+                        <a class="nav-link customfont" href="#">About Us</a>
                     </li>
-
-                <?php else : ?>
-
-                    <li class="nav-item">
-                        <a class="nav-link customfont" href="login.php">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link customfont" href="register.php">Register</a>
-                    </li>
-
-                <?php endif; ?>
-            </ul>
+                    <?php if (isset($_SESSION['auth_user'])) : ?>
+                        <li class="nav-item dropdown customfont">
+                            <a class="nav-link dropdown-toggle customfont" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <?= $_SESSION['auth_user']['user_name']; ?>
+                            </a>
+                            <ul class="dropdown-menu customfont" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item customfont font-style" href="#">My Profile</a></li>
+                                <li>
+                                    <form action="allcode.php" method="post">
+                                        <button type="submit" name="logout_btn" class="dropdown-item font-style" href="#">Logout</button>
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    <?php else : ?>
+                        <li class="nav-item">
+                            <a class="nav-link customfont" href="login.php">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link customfont" href="register.php">Register</a>
+                        </li>
+                    <?php endif; ?>
+                </ul>
+            </div>
+        </nav>
+        <div class="container">
+            <!-- Your container content goes here -->
         </div>
-</div>
-</nav>
+        <div></div>
+    </div>
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link active customfont font-style" id="nav-item" href="index.php">HOME</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link customfont font-style" id="nav-item" href="partners.php">PARTNERS</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link customfont font-style" id="nav-item" href="projects.php">PROJECTS</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link customfont font-style" id="nav-item" href="articles.php">ARTICLES</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link customfont font-style" id="nav-item" href="gallery.php">GALLERY</a>
+        </li>
+    </ul>
+</body>
 
-
-
-<div class="container">
-
-
-
-
-</div>
-
-
-
-<!-- Nav Center -->
-<ul class="nav justify-content-center">
-    <li class="nav-item">
-        <a class="nav-link active customfont" href="index.php">HOME</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link customfont" href="partners.php">PARTNERS</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link customfont" href="projects.php">PROJECTS</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link customfont" href="articles.php">ARTICLES</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link customfont" href="gallery.php">GALLERY</a>
-    </li>
-</ul>
+</html>
+</body>
