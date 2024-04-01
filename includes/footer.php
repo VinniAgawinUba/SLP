4b2e141771bdccab17264ea6f9c2871dc4d70057
C00L1N <jimenez.bencoolin@gmail.com>
2024-04-01 21:07:07 +0800

    Update footer.php
    styling headers for footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,70 +2,85 @@
 <script src="assets/js/bootstrap5.bundle.min.js"></script>
 <script src="assets/js/scripts.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.11.1/viewer.min.js"></script>
-                                        
-<script src="https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.11.1/viewer.min.js"></script>
-                                        
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.11.1/viewer.min.js"></script>
+
+<style>
+    #headers {
+        font-family: 'Inter';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 16px;
+        line-height: 19px;
+
+        color: #283971;
+    }
+</style>
 <!-- Page Content-->
 <section class="pt-4 bg-light bg-gradient" style="position: relative;">
-            <div class="container px-lg-5">
-                <!-- Page Features-->
-                <div class="row gx-lg-5" >
-                    <div class="col-lg-4 col-xxl-4 ">
-                        <div class="card bg-light border-0">
-                            <div class="card-body ">
-                                <h2 class="fs-4 fw-bold">CONTACT US</h2>
-                                <a href=""><p class="mb-0">rso@xu.edu.ph</p></a>
-                                <p class="mb-0">(088) 853-9800 | Local 9173</p>
-                            </div>
-                        </div>
+    <div class="container px-lg-5">
+        <!-- Page Features-->
+        <div class="row gx-lg-5">
+            <div class="col-lg-4 col-xxl-4 ">
+                <div class="card bg-light border-0">
+                    <div class="card-body ">
+                        <h2 class="fs-4 fw-bold" id="headers">CONTACT US</h2>
+                        <a href="">
+                            <p class="mb-0">rso@xu.edu.ph</p>
+                        </a>
+                        <p class="mb-0">(088) 853-9800 | Local 9173</p>
                     </div>
-                    <div class="col-lg-4 col-xxl-4">
-                        <div class="card bg-light border-0">
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-xxl-4">
-                        <div class="card bg-light border-0 ">
-                            <div class="card-body">
-                                
-                                <h2 class="fs-4 fw-bold">REQUEST ACCESS</h2>
-                                <p class="mb-2">Want more access to information?</p>
-                                <button type="button" class="btn btn-primary">Request Access</button>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-
-                <div class="row gx-lg-5">
-                <div class="col-lg-7 col-xxl-4">
-                        <div class="card bg-light border-0 h-100">
-                            <div class="card-body">
-    
-                                <h2 class="fs-4 fw-bold">FIND US HERE</h2>
-                                <p class="mb-0">Ground Floor, Agriculture Building, Room 103, Xavier University - </p>
-                                <p class="mb-0">Ateneo de Cagayan, Corrales Avenue,</p>
-                                <p class="mb-0">Cagayan de Oro City, 9000, Philippines</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-5 col-xxl-4 h-50">
-                        <div class="card bg-light border-0">
-                        </div>
-                    </div>
-                    <div class="col-lg-5 col-xxl-4 h-50">
-                        <div class="card bg-light border-0">
-                        </div>
-                    </div>
-                </div>
-                <hr>
-                <div class="row gy-3">
-                    <div class="col" style="color: #3A53A5; position:absolute; text-align: center;"><p>© 2023 Xavier University - Ateneo De Cagayan Corrales Avenue, Cagayan de Oro, Philippines</p></div>
-                    
                 </div>
             </div>
-        </section>
+            <div class="col-lg-4 col-xxl-4">
+                <div class="card bg-light border-0">
+                </div>
+            </div>
+            <div class="col-lg-4 col-xxl-4">
+                <div class="card bg-light border-0 ">
+                    <div class="card-body">
+
+                        <h2 class="fs-4 fw-bold" id="headers">REQUEST ACCESS</h2>
+                        <p class="mb-2">Want more access to information?</p>
+                        <button type="button" class="btn btn-primary">Request Access</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+        <div class="row gx-lg-5">
+            <div class="col-lg-7 col-xxl-4">
+                <div class="card bg-light border-0 h-100">
+                    <div class="card-body">
+
+                        <h2 class="fs-4 fw-bold" id="headers">FIND US HERE</h2>
+                        <p class="mb-0">Ground Floor, Agriculture Building, Room 103, Xavier University - </p>
+                        <p class="mb-0">Ateneo de Cagayan, Corrales Avenue,</p>
+                        <p class="mb-0">Cagayan de Oro City, 9000, Philippines</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5 col-xxl-4 h-50">
+                <div class="card bg-light border-0">
+                </div>
+            </div>
+            <div class="col-lg-5 col-xxl-4 h-50">
+                <div class="card bg-light border-0">
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="row gy-3">
+            <div class="col" style="color: #3A53A5; position:absolute; text-align: center;">
+                <p>© 2023 Xavier University - Ateneo De Cagayan Corrales Avenue, Cagayan de Oro, Philippines</p>
+            </div>
+
+        </div>
+    </div>
+</section>
 
 </body>
+
 </html>
