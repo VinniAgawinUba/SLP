6b6979095e7b1a027d7bc66ae25d27dd742359c5
Miguel Tan <j6xrmiguel@gmail.com>
2024-05-01 12:36:04 +0800

    updated slp db

--- slp.sql
+++ slp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 30, 2024 at 05:12 AM
+-- Generation Time: May 01, 2024 at 06:34 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `slp_db`
+-- Database: `slp`
 --
 
 -- --------------------------------------------------------
