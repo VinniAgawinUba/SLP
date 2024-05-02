612805cda58ecdc0532da4b9397950dbd3b999c8
C00L1N <200710422@my.xu.edu.ph>
2024-05-02 14:26:48 +0800

    Update login.php
    login new look

--- login.php
+++ login.php
@@ -1,9 +1,7 @@
 <?php
 session_start();
-if(isset($_SESSION['auth']))
-{
-    if(!isset($_SESSION['message']))
-    {
+if (isset($_SESSION['auth'])) {
+    if (!isset($_SESSION['message'])) {
         $_SESSION['message'] = "You are already logged in";
     }
     header('location: index.php');
@@ -13,35 +11,110 @@ if(isset($_SESSION['auth']))
 include('includes/header.php');
 include('includes/navbar.php');
 ?>
+<style>
+    .headers {
+        font-family: 'Inter';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 48px;
+        line-height: 58px;
+        text-align: center;
 
-<div class="py-5">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-5">
-                    
+        color: #283971;
+        margin-top: 99px;
+    }
+
+    .horizontal-line {
+        background-color: #283971;
+        width: 50%;
+        margin: auto;
+        border-top: 4px solid #283971 !important;
+        border-radius: 14px;
+        margin-top: 36px;
+        margin-bottom: 30px;
+    }
+
+    body {
+        margin-left: 100px;
+        margin-right: 100px;
+        height: 60em;
+    }
+
+    label {
+        font-family: 'Inter';
+        font-style: normal;
+        color: #283971;
+        font-weight: 700;
+        padding: 5px;
+    }
+
+    p {
+        font-family: 'Inter';
+        font-style: normal;
+        color: #283971;
+    }
+
+    ::placeholder {
+        color: gray;
+        font-style: normal;
+        font-weight: 400;
+    }
+
+    #Login-button {
+        width: 100px;
+        height: 32px;
+        left: 865px;
+        top: 2714px;
+        border: none;
+        background: #283971;
+        border-radius: 30px;
+        font-family: 'Inter';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 13px;
+        line-height: 16px;
+        text-align: center;
+        color: #FFFFFF;
+    }
+
+    #Login-button:hover {
+        background-color: #A19158;
+        transition: color 5s;
+    }
+
+    #Login-button:active {
+        background-color: #8D7F4D;
+    }
+</style>
+
+<body>
+    <h1 class="headers">LOGIN</h1>
+    <hr class="horizontal-line">
+    <div class="py-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-5">
                     <?php include('message.php'); ?>
-               <div class="card">
-                <div class="card-header">
-                    <h4>Login</h4>
+                    <div class="card">
+                        <div class="card-body">
+                            <form action="logincode.php" method="POST">
+                                <div class="form-group mb-3">
+                                    <label>Email ID</label>
+                                    <input required type="email" name="email" placeholder="Enter Email Address" class="form-control">
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label>Password</label>
+                                    <input required type="password" name="password" placeholder="Enter Password" class="form-control">
+                                </div>
+                                <div class="form-group mb-3">
+                                    <button required type="submit" name="login_btn" class="btn btn-primary" id="Login-button">Login Now</button>
+                                </div>
+                                <p>Don't have an account? <a href="register.php">Register Now</a></p>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <div class="card-body">
-                    <form action="logincode.php" method="POST">
-                    <div class="form-group mb-3">
-                        <label>Email ID</label>
-                        <input required type="email" name="email" placeholder="Enter Email Address" class="form-control">
-                    </div>
-                    <div class="form-group mb-3">
-                        <label>Password</label>
-                        <input required type="password" name="password" placeholder="Enter Password" class="form-control">
-                    </div>
-                    <div class="form-group mb-3">
-                        <button required type="submit" name="login_btn" class="btn btn-primary">Login Now</button>
-                    </div>
-                    <p>Don't have an account? <a href="register.php">Register Now</a></p>
-                    </form>
-
-                </div>
-               </div>
             </div>
-</div>
-
+        </div>
+    </div>
+</body>
